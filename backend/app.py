50731c197ab0434b5bc89f137831c7440a254814
Marcin Kielar <marcin.kielar@allegro.com>
2024-03-23 16:55:00 +0100

    SNIF-22 | Add mongo container and simple configuration

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 api = Api(app)
 swagger = Swagger(app)
 
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['test-database']
+collection = db['test-collection']
 
 class HelloWorld(Resource):
     def get(self):
@@ -28,6 +33,27 @@ class HelloWorld(Resource):
 
 
 api.add_resource(HelloWorld, '/')
+
+
+class InsertMongo(Resource):
+    def get(self):
+        """
+        Inserts a simple object into mongodb
+        """
+
+        post_data = {
+            'name': 'John Doe',
+            'email': 'example@example.com'
+        }
+
+        collection.insert_one(post_data)
+
+        return {'status': 'success'}
+
+
+api.add_resource(InsertMongo, '/insert')
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
