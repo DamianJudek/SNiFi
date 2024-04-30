706d0bed3b5d6c24ee07d3d241566bd69454af88
Marcin Kielar <marcin.kielar@allegro.com>
2024-04-30 22:06:36 +0200

    SNIF-24 | minor docker improvements

--- backend/app.py
+++ backend/app.py
@@ -16,8 +16,9 @@ swagger = Swagger(app)
 
 executor = ThreadPoolExecutor(max_workers=4)
 
-#TODO add condition to check if app is running in docker container or in local environment and change the host accordingly
-client = MongoClient('mongodb://mongodb:27017/')
+mongo_url = 'mongodb://mongodb:27017/' if os.environ.get('RUNNING_IN_DOCKER') else 'mongodb://localhost:27017/'
+
+client = MongoClient(mongo_url)
 db = client['snifi-db']
 scan_status_collection = db['scan_status']
 scan_result_collection = db['scan_result']
@@ -27,6 +28,16 @@ class StartScan(Resource):
     def post(self):
         """
         Starts a nmap scan of the network
+        ---
+        responses:
+          200:
+            description: Scan started successfully
+            schema:
+              properties:
+                scanId:
+                  type: string
+                  format: uuid
+                  description: Started scan id
         """
 
         uid = str(uuid.uuid4())
@@ -35,7 +46,7 @@ class StartScan(Resource):
 
         executor.submit(self.start_scan, uid)
 
-        return {'status': 'success', 'scanId': uid}
+        return {'scanId': uid}
 
     def start_scan(self, uid: str):
         print(f'Scan {uid} started')
@@ -60,12 +71,68 @@ class GetRunningScans(Resource):
     def get(self):
         """
         Gets a list of all running scans
+        ---
+        responses:
+          200:
+            description: List of running scans
+            schema:
+              properties:
+                scanId:
+                  type: string
+                  format: uuid
+                  description: Running scan id
+                startTime:
+                  type: string
+                  format: date-time
+                status:
+                  type: string
+                  enum: [running, completed, failed]
+                  example: running
+                endTime:
+                  type: string
+                  format: date-time
+                  nullable: true
+                  example: null
         """
 
         running_scans = list(scan_status_collection.find({'status': 'running'}, {'_id': False}))
-        print(running_scans)
-        print(datetime.now())
         return running_scans
+
+
+class GetAllScans(Resource):
+    def get(self):
+        """
+        Gets a list of all scans
+        ---
+        responses:
+          200:
+            description: List of all scans
+            schema:
+              type: array
+              items:
+                type: object
+                properties:
+                  scanId:
+                    type: string
+                    format: uuid
+                    description: Running scan id
+                  startTime:
+                    type: string
+                    format: date-time
+                  status:
+                    type: string
+                    enum: [running, completed, failed]
+                    example: completed
+                  endTime:
+                    type: string
+                    format: date-time
+                    nullable: true
+        """
+
+        all_scans = list(scan_status_collection.find({}, {'_id': False}))
+        print(all_scans)
+        print(datetime.now())
+        return all_scans
 
 
 class GetScanResult(Resource):
@@ -80,17 +147,36 @@ class GetScanResult(Resource):
 
 
 class MockScan(Resource):
-    def get(self):
+    def get(self, id: str):
         scan_status_collection.insert_one(
-            {'startTime': str(datetime.now()), 'scanId': '1', 'status': 'running', 'endTime': None}
+            {'startTime': str(datetime.now()), 'scanId': id, 'status': 'running', 'endTime': None}
         )
-        return {'status': 'success', 'scanId': '0'}
+        return {'status': 'success', 'scanId': id}
+
+
+class UpdateScan(Resource):
+    def post(self, id: str):
+        scan_status_collection.update_one(
+            {'scanId': id},
+            {'$set': {'status': 'completed', 'endTime': str(datetime.now())}}
+        )
+        return {'status': 'success'}
+
+
+class DeleteAllScans(Resource):
+    def delete(self):
+        scan_status_collection.delete_many({})
+        scan_result_collection.delete_many({})
+        return {'status': 'success'}
 
 
 api.add_resource(StartScan, '/start_scan')
 api.add_resource(GetRunningScans, '/running_scans')
+api.add_resource(GetAllScans, '/all_scans')
 api.add_resource(GetScanResult, '/scan_result/<string:scan_id>')
-api.add_resource(MockScan, '/mock_scan')
+api.add_resource(MockScan, '/mock_scan/<string:id>')
+api.add_resource(UpdateScan, '/update_scan/<string:id>')
+api.add_resource(DeleteAllScans, '/delete_all_scans')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
