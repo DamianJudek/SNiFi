f42ebda48d71a88f208ead458b8890a6280763fc
damian-judek-wttech <damian.judek@wundermanthompson.com>
2024-05-31 10:48:17 +0200

    SNIF-55: fetch notification in interval

--- frontend/src/components/NotificationIcon/NotificationIcon.tsx
+++ frontend/src/components/NotificationIcon/NotificationIcon.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import Badge from "@mui/material/Badge";
+import { Link } from "react-router-dom";
+import { getNotifications } from "../../api";
 import Icon from "../Icon/Icon";
 import { Wrapper } from "./NotificationIcon.styled";
-import { Link } from "react-router-dom";
 
 const Notifications = () => {
+  const [notificationsCount, setNotificationsCount] = useState<number>(0);
+
+  const fetchNotifications = () => {
+    getNotifications()
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        }
+        throw res.json();
+      })
+      .then((res) => {
+        setNotificationsCount(res?.new?.length || 0);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setTimeout(fetchNotifications, 10000);
+      });
+  };
+
+  useEffect(fetchNotifications, []);
+
   return (
     <Wrapper>
       <Link to="/notifications">
-        <Badge badgeContent={4} color="primary">
+        <Badge badgeContent={notificationsCount} color="primary">
           <Icon name="flag" />
         </Badge>
       </Link>
